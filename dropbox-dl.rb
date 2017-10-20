65b0b1a7ff922ec2471ecc95f509331f4a5412de
Josh McGrath <josh.bernard.mcgrath@gmail.com>
2017-10-20 19:47:29 -0300

    update dropbox-dl formula to 0.0.3

--- dropbox-dl.rb
+++ dropbox-dl.rb
@@ -1,8 +1,8 @@
 class DropboxDl < Formula
   desc "CLI tool to recursively download public Dropbox folders"
   homepage "https://github.com/jbmcg/dropbox-dl"
-  url "https://github.com/jbmcg/dropbox-dl/archive/0.0.2.tar.gz"
-  sha256 "464876656c0c7c8938b1a4ca3e0b75ac1b87800dbbb1f84870a1dcd7e6183725"
+  url "https://github.com/jbmcg/dropbox-dl/archive/0.0.3.tar.gz"
+  sha256 "02a162bf70881ca3287b96d388ecfa2ad1dd1f17030e552d38fc90a292d0c4d2"
   head "https://github.com/jbmcg/dropbox-dl.git"
 
   def install
